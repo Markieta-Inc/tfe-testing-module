d2eb1159c189b115e373bdf4d124d28932829a04
Christopher Markieta <markietachristopher@gmail.com>
2020-07-06 19:33:39 -0400

    Reset back to before.

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
 module "testing-provisioner" {
-  source = "github.com/Markieta-Inc/tfe-testing-provisioners.git?ref=v0.0.1"
-}
-
-module "testing-provisioner-2" {
   source = "github.com/Markieta-Inc/tfe-testing-provisioners.git"
 }
