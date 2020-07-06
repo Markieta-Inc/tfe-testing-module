e8e491a81ae9c95fd57abfc0fe63f62cdda03837
Christopher Markieta <markietachristopher@gmail.com>
2020-07-06 17:41:27 -0400

    Setting 2 module calls, 1 correct,.

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
 module "testing-provisioner" {
+  source = "github.com/Markieta-Inc/tfe-testing-provisioners.git?ref=v0.0.1"
+}
+
+module "testing-provisioner-2" {
   source = "github.com/Markieta-Inc/tfe-testing-provisioners.git"
 }
